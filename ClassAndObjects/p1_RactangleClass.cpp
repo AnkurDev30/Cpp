ea884ed0004ad0e68fd203c7c2a904150d8e3376
AnkurDev30 <ankurchoure30nov@gmail.com>
2026-02-06 22:48:15 +0530

    comments added

--- ClassAndObjects/p1_RactangleClass.cpp
+++ ClassAndObjects/p1_RactangleClass.cpp
@@ -1,5 +1,5 @@
 //Rectangle Class
-
+/************************************************************************** */
 /*Create a class Rectangle with:
 
 length
@@ -12,41 +12,57 @@ read()
 area()
 
 perimeter()*/
-
+/************************************************************************** */
+//!< header files.
 #include<iostream>
+//!< namespace defination, 
 namespace nRactangle
 {
+    //!< class for ractangle.
     class cRactangle
     {
         private:
+            //!< data members
             float fLength;
             float fBreadth;
         public:
+            //!< member functions 
             void vReadDataFun();
             void vAreaFun();
             void vPerimeterFun();
     };
 }
+//!< Read Length and Breadth
 void nRactangle::cRactangle::vReadDataFun()
 {
     std::cout<<"enter length and breadth"<<std::endl;
 
     std::cin>>fLength>>fBreadth;
 }
+//!< Calculating area.
 void nRactangle::cRactangle::vAreaFun()
 {
     float fArea = fLength * fBreadth;
     std::cout<<"area of ractangle = "<<fArea<<std::endl;
 }
+//!< Calculating Perimeter
 void nRactangle::cRactangle::vPerimeterFun()
 {
     float fPerimeter = 2*(fLength + fBreadth);
     std::cout<<"area of ractangle = "<<fPerimeter<<std::endl;
 }
+//!< main Function
 int main()
 {
+    //!< object creation.
     nRactangle::cRactangle objRactangle;
+
+    //!< calling vReadDataFun function
     objRactangle.vReadDataFun();
+
+    //!< calling vAreaFun function
     objRactangle.vAreaFun();
+
+    //!< calling vPerimeterFun function  
     objRactangle.vPerimeterFun();
 }
